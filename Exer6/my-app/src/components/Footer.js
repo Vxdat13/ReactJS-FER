767e314ecba405af48b3c6ad3539441dfb420811
Vxdat13 <vxdat13@gmail.com>
2023-06-03 03:25:24 +0700

    Review1

---
+++ Exer6/my-app/src/components/Footer.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import './Footer.css'
+export default class footer extends Component {
+    render() {
+        return (
+            <div className="Footer">
+                copyright ©️ 2020
+            </div>
+        )
+    }}
